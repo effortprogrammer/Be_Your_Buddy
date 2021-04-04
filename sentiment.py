4c3f3cc35a1204c90d473567a2923da65e792fd6
effortprogrammer <yhjhoward7@gmail.com>
2021-04-04 09:06:45 -0700

    upload files

---
+++ sentiment.py
@@ -0,0 +1,31 @@
+import os, requests, uuid, json
+
+# Don't forget to replace with your Cog Services subscription key!
+subscription_key = '75fd7ddabbbb4055a59ce6d9b30f988f' 
+# Our Flask route will supply four arguments: input_text, input_language,
+# output_text, output_language.
+# When the run sentiment analysis button is pressed in our Flask app,
+# the Ajax request will grab these values from our web app, and use them
+# in the request. See main.js for Ajax calls.
+
+def get_sentiment(input_text, input_language, output_text, output):
+    constructed_url = 'https://westus2.api.cognitive.microsoft.com/text/analytics/v3.1-preview.1/sentiment'
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = {
+        'documents': [
+            {
+                'language': input_language,
+                'id': '1',
+                'text': input_text
+            },
+        ]
+    }
+    response = requests.post(constructed_url, headers=headers, json=body)
+    return response.json()
